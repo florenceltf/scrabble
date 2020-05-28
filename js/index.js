ea3deb4f5259193632b364b8a9653b5f386fcd21
Florence Li <florenceli@Florences-MBP.gateway>
2020-05-28 11:38:11 +1000

    js update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    $("html").mousemove(function (event) {
+        $("#cursor").css("transform", "translate3d(" + (event.clientX - 25) + "px," + (event.clientY - 25) + "px,0px)")
+    });
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $("#cursor").css("display", "none");
+    };
+
     var letterDropdown = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     letterDropdown.forEach(function (letter) {
@@ -154,13 +162,5 @@ $(document).ready(function () {
         $("#information").show();
         $("#overlay").toggle("slide", { direction: "left" }, 500);
     });
-
-    $("html").mousemove(function (event) {
-        $("#cursor").css("transform", "translate3d(" + (event.clientX - 25) + "px," + (event.clientY - 25) + "px,0px)")
-    });
-
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        $("#cursor").css("display", "none");
-    };
 
 });
